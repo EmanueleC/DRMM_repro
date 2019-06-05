8397ff4b29270f522d2e59f54b895c3476ba3a7f
EmanueleC <emanuele.carraro94@gmail.com>
2019-06-05 18:39:39 +0200

    up dataset_analysis

--- utilities/data_analysis.py
+++ utilities/data_analysis.py
@@ -4,11 +4,11 @@ import json
 import matplotlib.pyplot as plt
 
 
-def print_info_length(data, name_list, attr, plot):
+def print_info_length(labels, data, name_list, attr, plot):
     stats = "\nLength of data in " + name_list + ": " + str(len(data))
     if plot:
         plt.clf()
-        plt.plot(range(len(data)), data, linestyle='None', marker='x')
+        plt.plot(labels, data, linestyle='None', marker='x')
         plt.title(name_list + attr)
         plt.xlabel("Observations")
         plt.ylabel('# ' + attr)
@@ -45,7 +45,12 @@ text += "Total documents: " + str(len(corpus_obj.docs.keys()))
 text += "\nTotal queries: " + str(len(queries_obj.keys()))
 
 lines_corpus_splitted = [len(set(line)) for line in corpus_obj]
+corpus_labels = [doc.document_id for doc in corpus_obj.docs.values()]
+corpus_labels.sort()
+lines_corpus_splitted = [x for _, x in sorted(zip(corpus_labels, lines_corpus_splitted))]
 lines_queries_splitted = [len(set(line.split())) for query in queries_obj.values() for line in query.get_lines()]
+queries_labels = [query.qId for query in queries_obj.values()]
+lines_queries_splitted = [x for _, x in sorted(zip(queries_labels, lines_queries_splitted))]
 
 max_lines_queries = max([len(query.get_lines()) for query in queries_obj.values()])
 max_lines_docs = max([len(doc.get_lines()) for doc in corpus_obj.docs.values()])
@@ -58,8 +63,8 @@ text += "\nLongest document text: " + str(max(lines_corpus_splitted)) + " words"
 text += "\nShortest query text: " + str(min(lines_queries_splitted)) + " words"
 text += "\nLongest query text: " + str(max(lines_queries_splitted)) + " words"
 
-text += print_info_length(lines_corpus_splitted, "corpus docs" + conf, "words", True)
-text += print_info_length(lines_queries_splitted, "queries" + conf, "words", True)
+text += print_info_length(corpus_labels, lines_corpus_splitted, "corpus docs" + conf, "words", True)
+text += print_info_length(queries_labels, lines_queries_splitted, "queries" + conf, "words", True)
 
 text += '\n' + str(corpus_model)
 
