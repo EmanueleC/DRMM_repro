d34adad98725816edfae2b83c9e1ff6c09739219
Emanuele <emanuele.carraro.94@gmail.com>
2019-07-03 14:55:15 +0200

    Delete pre_script.sh

--- pre_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python -m preprocessing.pre
-#python -m comparison.getRunFromFile
-python -m preprocessing.stem_stopwords -sw -st
-python -m preprocessing.tf-idf -sw -st
-python -m preprocessing.prepare_ids -topic=-1
-python -m preprocessing.embeddings -sw -st
-python -m preprocessing.encoding -sw -st
